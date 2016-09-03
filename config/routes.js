78e6734b880ef2f649ad01478a23c16498baa87e
NatasaPeic <peic.natasa@gmail.com>
2016-09-03 13:14:31 -0400

    Add routes for admin

--- config/routes.js
+++ config/routes.js
@@ -23,11 +23,11 @@ module.exports = require('lib/wiring/routes')
 
 
 //admins
-// .post('/sign-up', 'admins#signup')
-// .post('/sign-in', 'admins#signin')
-// .delete('/sign-out/:id', 'admins#signout')
-// .patch('/change-password/:id', 'admins#changepw')
-// .resources('admins', { only: ['index', 'show'] })
+.post('/sign-up', 'admins#signup')
+.post('/sign-in', 'admins#signin')
+.delete('/sign-out/:id', 'admins#signout')
+.patch('/change-password/:id', 'admins#changepw')
+.resources('admins', { only: ['index', 'show'] })
 
 // users of the app have special requirements
 .post('/sign-up', 'users#signup')
