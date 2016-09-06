16ed9294ba35234b3f893f0ea42f02b2ebae0a09
alex-braun <braunacb@gmail.com>
2016-09-06 11:29:57 -0400

    Add validation message to orders model.
    Validates that total from front end === price * quantity.
    Validates that the front end design is correct.

--- app/models/order.js
+++ app/models/order.js
@@ -8,20 +8,20 @@ const orderSchema = new mongoose.Schema({
       default: [],
       required: true,
   },
-      total_price: {
+      total: {
         type: Number,
         required: true,
   },
       paid: {
         type: Boolean,
     },
-      date_purchased: {
+      date_completed: {
         type: Date,
         match: /\d{4}-\d{2}-\d{2}/,
     },
       token: {
         type: String,
-        required: true,
+        // required: true,
       },
     _owner: {
       type: mongoose.Schema.Types.ObjectId,
@@ -30,12 +30,26 @@ const orderSchema = new mongoose.Schema({
   },
 }, {
   timestamps: true,
-  // toJSON: { virtuals: true },
-});
+  toJSON: { virtuals: true },
+}
+);
 
-// orderSchema.virtual('length').get(function length() {
-//   return this.text.length;
-// });
+  orderSchema.virtual('total_validation').get(function() {
+   let front_total = this.total;
+   if (!this.total) {
+     return 'Error';
+   }
+   let itemsArray = this.items;
+   let sum = 0;
+   for(let i=0; i<itemsArray.length; i++){
+     sum += itemsArray[i].price * itemsArray[i].quantity;
+   }
+   if (front_total === sum ){
+     return true;
+   } else {
+     return 'Error';
+   }
+});
 
 const Order = mongoose.model('Order', orderSchema);
 
