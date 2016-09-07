e4c6268322b22b50c894fbe140538393470a63ab
NatasaPeic <peic.natasa@gmail.com>
2016-09-07 11:25:10 -0400

    Add mongoLab URI

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/ecommerce-back';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost/ecommerce-back';
+// const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/ecommerce-back';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
